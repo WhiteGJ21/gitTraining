5170701d656f131b9913e9cd939842aee3313408
WhiteGJ21 <WhiteGJ21@gcc.edu>
2024-02-19 14:37:11 -0500

    Yoda Quote

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("I like the simple things in life like dynamite and gunpowder");
         System.out.println("You merely adapted the dark, I was born in it, molded by it");
-        System.out.println("Only 2 there are, a master and an apprentice");
+        System.out.println("Only 2 there are, a master and an apprentice.");
     }
 }
