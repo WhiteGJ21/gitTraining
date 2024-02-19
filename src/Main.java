642f5f2177dd45e88fc2ada2aaa7de3aa3b0cc7a
WhiteGJ21 <WhiteGJ21@gcc.edu>
2024-02-19 14:30:02 -0500

    Bane Quote

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I like the simple things in life like dynamite and gunpowder");
+        System.out.println("You merely adapted the dark, I was born in it, molded by it");
     }
 }
